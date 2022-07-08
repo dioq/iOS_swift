8cf2a8a83780398fd59092e90fc263efec1400e2
Dioq <zhendong2011@live.cn>
2022-07-09 05:16:48 +0800

    原生网络

--- MySwift/mycode/URLSession/URLSessionVC.swift
+++ MySwift/mycode/URLSession/URLSessionVC.swift
@@ -20,6 +20,7 @@ class URLSessionVC: UIViewController {
     }
     
     @IBAction func get_func(_ sender: UIButton) {
+        self.showLB.text = ""
         let urlStr:String = "http://jobs8.cn:8081/getdata"
         let url = URL.init(string: urlStr)
         var request = URLRequest(url: url!)
@@ -58,6 +59,7 @@ class URLSessionVC: UIViewController {
     }
     
     @IBAction func post_func(_ sender: UIButton) {
+        self.showLB.text = ""
         let urlStr = "http://jobs8.cn:8081/postdata"
         let url = URL(string: urlStr)
         var request = URLRequest.init(url: url!)
@@ -102,6 +104,7 @@ class URLSessionVC: UIViewController {
     }
     
     @IBAction func formdata_func(_ sender: UIButton) {
+        self.showLB.text = ""
         let urlStr = "http://jobs8.cn:8081/formdata"
         let url = URL(string: urlStr)
         var request = URLRequest.init(url: url!)
@@ -135,6 +138,7 @@ class URLSessionVC: UIViewController {
     }
     
     @IBAction func downloadTask_func(_ sender: UIButton) {
+        self.showLB.text = ""
         //1.创建会话对象
         let config:URLSessionConfiguration=URLSessionConfiguration.default
         let session:URLSession = URLSession.init(configuration: config, delegate: self, delegateQueue: OperationQueue.main)
@@ -164,6 +168,7 @@ class URLSessionVC: UIViewController {
     }
     
     @IBAction func uploadTask_func(_ sender: Any) {
+        self.showLB.text = ""
         let urlStr = "http://jobs8.cn:8081/upload"
         let url = URL.init(string: urlStr)
         //创建请求
@@ -225,6 +230,7 @@ class URLSessionVC: UIViewController {
     }
     
     @IBAction func uploadBinary_func(_ sender: UIButton) {
+        self.showLB.text = ""
         let urlStr = "https://upload.gmugmu.com/api/v1/base/resource/image/upload?source_info=eyJhcHBpZCI6IjI2MDAwMyIsInVpZCI6IjMyNDk3MjMyIiwicGFnZSI6ImNvbS5lb21jaGF0Lm1vZHVsZS5ob21lLkhvbWVQYWdlQWN0aXZpdHkiLCJ0aW1lIjoiMTYwNjI0Mjc3MDA5NiJ9&cc=TG73257&dev_name=nubia&oaid=&cpu=[Adreno_(TM)_630][ARMv7_639_placeholder]&lc=37427d9d8660d3f7&osversion=android_22&sid=02Yok7jQeFpBTR+Uz1tDaONajE3oi6XdrdqPwUyM/joBe9tJRE6grYwCweYmrwWh&ndid=&conn=wifi&ram=3650129920&msid=363635333236303436313030303634&icc=89860081133720371180&statistics=9ad290c3317d39cc8be58b98f74c86e7&mtid=e6352e4b164246b8b0be20f9c36f5abe&atid=302e30&tourist=&ongd=302e30&mtxid=00812dc6db02&evid=3535303335643264616635632d343464382d626465332d373262342d3034366638353761&cv=GM4.6.30_Android&proto=&logid=2006201,2001902,2002202,201802,202101,204005,204401,2005402,2006102,2001701,2003301,2005601,2005502,2001501,2005202,204301,2001802,2005801,2003502,2004701,2000802,201007,2001602,2004901,2002101,2003802,2005902,204101,2006001,2004801,2001202&ua=nubiaNX629J&uid=32497232&vv=202010141815&meid=363834373537373230363631353638&smid=&aid=b35517406764bf51"
         let url = URL.init(string: urlStr)
         //创建请求
