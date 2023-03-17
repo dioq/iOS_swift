abfe4244d6c273298d0eb17fcc7253b4fac45efc
Dioq <zhendong2011@live.cn>
2023-03-17 18:42:14 +0800

    动画中的一个新理解

--- MySwift/ui/animations/AnimationVC.swift
+++ MySwift/ui/animations/AnimationVC.swift
@@ -14,19 +14,21 @@ class AnimationVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.title = "动画"
+        self.navigationItem.title = "动画"
         
-        self.view2 = UIView.init(frame: CGRect(x: 250, y: 150, width: 100, height: 100))
+        self.view2 = UIView.init(frame: CGRect(x: 10, y: 10, width: 100, height: 100))
         self.view2.backgroundColor = UIColor.red
         self.view.addSubview(view2)
-        let opts = UIView.AnimationOptions.autoreverse
+        let opts = UIView.AnimationOptions.autoreverse // 执行完动画后,自动逆向执行一次
         
-        UIView.animate(withDuration: 5, delay: 0, options: opts, animations: {
-            self.view2.center.x -= 100
+        UIView.animate(withDuration: 5, delay: 5, options: opts, animations: {
+            print("file:",#file,"\tfunction:",#function,"\tline:",#line)
+            self.view2.frame = CGRect(x: 355, y: 600, width: 20, height: 20)
         }) { (make) in
+            print("file:",#file,"\tfunction:",#function,"\tline:",#line)
             print(make)//此处要等动画执行完后才执行
         }
-        print("go here!! =========== 22222222")
+        print("file:",#file,"\tfunction:",#function,"\tline:",#line)
     }
-
+    
 }
